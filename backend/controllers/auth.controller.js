59ca2dcfc1b0990cbb23e5bf7eea647e8ac6c914
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-11-21 00:00:20 +0530

    AuthController Modification

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -154,4 +154,13 @@ export const refreshToken = async(req,res) => {
     }
 };
 
-//TODO implement get profile later
+export const getProfile = async function (req, res) {
+    try {
+        res.json(req.user);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message});
+    }
+    
+}
+
+
