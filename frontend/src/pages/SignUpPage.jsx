c8013667a5812e6f398dd31774aa0651481f41e3
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-11-22 01:09:44 +0530

    Signup Page-(LoginPage path error fixed)

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -146,7 +146,7 @@ const SignUpPage = () => {
           </form>
           <p className="mt-8 text-center text-sm text-gray-400">
             Already have an account?{" "}
-            <Link to="login" className="font-medium text-emerald-400 hover:text-emerald-300">
+            <Link to="/login" className="font-medium text-emerald-400 hover:text-emerald-300">
               Login here <ArrowRight className="inline h-4 w-4" />
             </Link>
           </p>
