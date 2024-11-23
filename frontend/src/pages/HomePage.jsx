581797f56b4338cb3599e7765eb86b24e0e193c5
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-11-24 03:36:03 +0530

    Display category items on homepage with grid layout

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,44 @@
+import CategoryItem from "../components/CategoryItem";
 
+
+
+const categories = [
+  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg"},
+  { href: "/tshirts", name: "T-shirts", imageUrl: "/t-shirts.jpg"},
+  { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg"},
+  { href: "/glasses", name: "Glasses", imageUrl: "/glasses.jpg"},
+  { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg"},
+  { href: "/suits", name: "Suits", imageUrl: "/suits.jpg"},
+  { href: "/bags", name: "Bags", imageUrl: "/bags.jpg"},
+];
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="relative min-h-screen text-white overflow-hidden">
+      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:p-6 lg:px-8 py-16">
+        <h1 className="text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4">
+        Categories at a Glance
+        </h1>
+        <p className="text-center text-xl text-gray-300 mb-12">
+        Explore the latest in eco-friendly fashion trends
+        </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {categories.map(category => (
+            <CategoryItem
+            category={category}
+            key={category.name}
+            />
+          ))}
+
+        </div>
+
+
+
+      </div>
+
+    </div>
+    
   )
 }
 
-export default HomePage
+export default HomePage;
