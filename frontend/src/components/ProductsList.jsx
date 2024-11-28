624aa951349ba1add5caae7e68e9f31c4e75866b
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-11-28 23:50:18 +0530

    Add ProductList component with delete and toggle feature functionality

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -1,6 +1,11 @@
-
+import { motion } from "framer-motion";
+import { Trash, Star } from "lucide-react";
+import { useProductStore } from '../stores/useProductStore';
 
 const ProductList = () => {
+  const {deleteproduct, toggleFeatureProduct, products } = useproductStore();
+  console.log("products", products);
+
   return (
     <div>ProductList</div>
   )
