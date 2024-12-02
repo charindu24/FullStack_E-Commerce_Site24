c3c7ccd8f07f87dc289beedf2d9eb5e8dcbc8468
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-12-02 20:35:06 +0530

    remove import { div } from "framer-motion/client";

--- frontend/src/components/GiftCouponCard.jsx
+++ frontend/src/components/GiftCouponCard.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { useCartStore } from "../stores/useCartStore";
-import { div } from "framer-motion/client";
+
 
 
 
